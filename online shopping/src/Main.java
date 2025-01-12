a243fd75644779189b51ec41a5c404b7cbd53c42
Parmin <350791315@gapps.yrdsb.ca>
2025-01-12 13:34:07 -0500

    Created the project
    made the category, prices and stock

--- online shopping/src/Main.java
+++ online shopping/src/Main.java
@@ -9,21 +9,105 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome To parmina online shop");
         System.out.println("Please enter the category you want to Enter ");
+        System.out.println("1.Clothing");
+        System.out.println("2.Books");
+        System.out.println("3.Shoes");
+        int categoryChoiceInt = sc.nextInt();
 
 
 
+        if (categoryChoiceInt == 1) {
+            System.out.println("Welcome to Clothing");
+            System.out.println("Here are the clothing items available");
+
+            System.out.println("1. Shirt - $19.99 ( Stock : 10)");
+            System.out.println("2. T-Shirt - $15.99 ( Stock : 5");
+            System.out.println("3. Jacket - $49.99 ( Stock : 3");
+
+            System.out.println("Select a product (1-3):");
+            int productChoiceInt = sc.nextInt();
 
 
-        System.out.println("Are a user or admin");
-        String userType = sc.nextLine().toLowerCase();
+            System.out.println("Enter quanity you want");
+            int quantity = sc.nextInt();
+            int shirtStock = 10;
+            int tShirtSock = 5;
+            int jacketSock = 3;
 
-        if (userType.equals("admin")) {
-            System.out.println("Welcome Admin");
-        } else if (userType.equals("user")) {
-            System.out.println("Welcome User");
+            if (productChoiceInt == 1 && quantity <= shirtStock) {
+                System.out.println("Added" + quantity + " shirt(s) to your cart");
+                shirtStock -= quantity;
+            } else if (productChoiceInt == 2 && quantity <= tShirtSock) {
+                System.out.println("Added " + quantity + " T-shirt(s) to your cart");
+                tShirtSock -= quantity;
+            } else if (productChoiceInt == 3 && quantity <= jacketSock) {
+                System.out.println("Added " + quantity + " Jacket(s) to your cart");
+                jacketSock -= quantity;
+            } else {
+                System.out.println("Invalid product choice");
+            }
+
+
+            } else if (categoryChoiceInt == 2) {
+            System.out.println("Welcome to Books");
+            System.out.println("Here are the book items available");
+            System.out.println("1. Harry Potter - $35.99");
+            System.out.println("2. The midnight library - $19.99");
+            System.out.println("3. You - $25.99");
+
+            System.out.println("Select a product (1-3):");
+            int productChoiceInt = sc.nextInt();
+
+            System.out.println("Enter quanity you want");
+            int quantity = sc.nextInt();
+            int HarryPotter= 10;
+            int midnightLibrary = 5;
+            int You = 3;
+            if (productChoiceInt == 1 && quantity <= HarryPotter) {
+                System.out.println("Added" + quantity + " Harry Potter");
+                HarryPotter -= quantity;
+            } else if (productChoiceInt == 2 && quantity <= midnightLibrary) {
+                System.out.println("Added " + quantity + " Midnight Library");
+                midnightLibrary -= quantity;
+            } else if (productChoiceInt == 3 && quantity <= You) {
+                System.out.println("Added " + quantity + " You");
+                You -= quantity;
+            } else {
+                System.out.println("Invalid product choice");
+            }
+
+
+        } else if (categoryChoiceInt == 3) {
+            System.out.println("Welcome to Shoes");
+            System.out.println("Here are the shoe items available");
+            System.out.println("1. Running shoes - $79.99");
+            System.out.println("2. Sneakers - $59.99");
+            System.out.println("3. Hills - $35.99");
+
+            System.out.println("Select a product (1-3):");
+            int productChoiceInt = sc.nextInt();
+            System.out.println("Enter quanity you want");
+            int quantity = sc.nextInt();
+            int RunningShoes= 10;
+            int Sneakers = 5;
+            int Hills = 2;
+            if (productChoiceInt == 1 && quantity <= RunningShoes) {
+                System.out.println("Added" + quantity + " Running Shoes");
+                RunningShoes -= quantity;
+            } else if (productChoiceInt == 2 && quantity <= Sneakers) {
+                System.out.println("Added " + quantity + " Sneakers");
+                Sneakers -= quantity;
+            } else if (productChoiceInt == 3 && quantity <= Hills) {
+                System.out.println("Added " + quantity + " Hills");
+                Hills -= quantity;
+            }
+        } else {
+            System.out.println("Invalid choice, please try again");
         }
+        sc.close();
     }
 }
+
 
 
 
